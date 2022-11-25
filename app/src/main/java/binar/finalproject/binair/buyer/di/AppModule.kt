c44295afd8c2e05a590c1f7bc3a2f4847d71098a
Richard Lois <richard.lois@ti.ukdw.ac.id>
2022-11-25 16:04:58 +0700

    Add retrofit config

---
+++ app/src/main/java/binar/finalproject/binair/buyer/di/AppModule.kt
@@ -0,0 +1,11 @@
+package binar.finalproject.binair.buyer.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+class AppModule {
+
+}
