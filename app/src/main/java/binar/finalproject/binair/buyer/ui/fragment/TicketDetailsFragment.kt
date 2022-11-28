dee15de88c2456de62463cce963df28731aaae20
Richard Lois <richard.lois@ti.ukdw.ac.id>
2022-11-28 20:54:06 +0700

    Update layouting for detail ticket

--- app/src/main/java/binar/finalproject/binair/buyer/ui/fragment/TicketDetailsFragment.kt
+++ app/src/main/java/binar/finalproject/binair/buyer/ui/fragment/TicketDetailsFragment.kt
@@ -5,16 +5,37 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import binar.finalproject.binair.buyer.R
+import binar.finalproject.binair.buyer.data.response.TicketItem
+import binar.finalproject.binair.buyer.databinding.FragmentTicketDetailsBinding
 
 class TicketDetailsFragment : Fragment() {
+    private lateinit var binding : FragmentTicketDetailsBinding
+    private lateinit var clickedTicket : TicketItem
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ticket_details, container, false)
+        binding = FragmentTicketDetailsBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        getDataFromBundle()
+        setDataToView()
+    }
+
+    private fun getDataFromBundle() {
+        clickedTicket = arguments?.getParcelable("clickedTicket")!!
+    }
+
+    private fun setDataToView() {
+        val oldDate = clickedTicket.date.substring(0, 10)
+        val formatedDate = oldDate.substring(8, 10) + "/" + oldDate.substring(5, 7) + "/" + oldDate.substring(0, 4)
+        clickedTicket.date = formatedDate
+        binding.ticket = clickedTicket
+    }
 }
