5ffe35f1e04f67e64e74b04b871d50f43d95e1f3
Richard Lois <richard.lois@ti.ukdw.ac.id>
2022-11-30 20:07:05 +0700

    Add global navigation for bottom nav

--- app/src/main/java/binar/finalproject/binair/buyer/ui/activity/MainActivity.kt
+++ app/src/main/java/binar/finalproject/binair/buyer/ui/activity/MainActivity.kt
@@ -2,6 +2,8 @@ package binar.finalproject.binair.buyer.ui.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import binar.finalproject.binair.buyer.R
 import binar.finalproject.binair.buyer.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -13,5 +15,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setBottomNavListener()
+    }
+
+    private fun setBottomNavListener() {
+        binding.bottomNav.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.navBuyTicket -> {
+                    findNavController(R.id.fragmentContainerView).navigate(R.id.action_global_homeFragment)
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.navProfile -> {
+                    findNavController(R.id.fragmentContainerView).navigate(R.id.action_global_profileFragment2)
+                    return@setOnNavigationItemSelectedListener true
+                }
+                else -> false
+            }
+        }
     }
 }
--- app/src/main/java/binar/finalproject/binair/buyer/ui/fragment/HomeFragment.kt
+++ app/src/main/java/binar/finalproject/binair/buyer/ui/fragment/HomeFragment.kt
@@ -1,6 +1,5 @@
 package binar.finalproject.binair.buyer.ui.fragment
 
-import android.R
 import android.annotation.SuppressLint
 import android.app.DatePickerDialog
 import android.app.DatePickerDialog.OnDateSetListener
@@ -10,7 +9,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
@@ -158,11 +156,11 @@ class HomeFragment : Fragment() {
     }
 
     private fun setAutoCompleteClass() {
-        val ticketsClass = arrayOf("Ekonomi","Bisnis","First Class")
-        val adapter: ArrayAdapter<String> = ArrayAdapter<String>(requireContext(), R.layout.select_dialog_item, ticketsClass)
-        val atcClas = binding.etKelasInput
-        atcClas.threshold = 1
-        atcClas.setAdapter(adapter)
+//        val ticketsClass = arrayOf("Ekonomi","Bisnis","First Class")
+//        val adapter: ArrayAdapter<String> = ArrayAdapter<String>(requireContext(), R.layout.select_dialog_item, ticketsClass)
+//        val atcClas = binding.etKelasInput
+//        atcClas.threshold = 1
+//        atcClas.setAdapter(adapter)
     }
 
     private fun showBottomNavigation() {
