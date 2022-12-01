c6fc195de193da2332b04e89c6ecd19d6a2997ec
aqbar <akbar.maulana000501@gmail.com>
2022-12-01 23:09:01 +0700

    membuat UI payment Rounded Trip

---
+++ app/src/main/java/binar/finalproject/binair/buyer/ui/fragment/PaymentRoundedTripFragment.kt
@@ -0,0 +1,21 @@
+package binar.finalproject.binair.buyer.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import binar.finalproject.binair.buyer.R
+
+
+class PaymentRoundedTripFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_payment_rounded_trip, container, false)
+    }
+
+}
