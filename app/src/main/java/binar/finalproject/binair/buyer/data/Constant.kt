9505d11db4ca5647c8200051ea7542a0492dcffd
Richard Lois <richard.lois@ti.ukdw.ac.id>
2022-12-02 10:59:02 +0700

    Add navigation on splash screen and other

--- app/src/main/java/binar/finalproject/binair/buyer/data/Constant.kt
+++ app/src/main/java/binar/finalproject/binair/buyer/data/Constant.kt
@@ -3,4 +3,5 @@ package binar.finalproject.binair.buyer.data
 object Constant {
     const val dataPassenger : String = "dataPassenger"
     const val dataUser : String = "dataUser"
+    const val initApp : String = "initApp"
 }
--- app/src/main/java/binar/finalproject/binair/buyer/ui/fragment/SplashScreenFragment.kt
+++ app/src/main/java/binar/finalproject/binair/buyer/ui/fragment/SplashScreenFragment.kt
@@ -1,20 +1,45 @@
 package binar.finalproject.binair.buyer.ui.fragment
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import binar.finalproject.binair.buyer.R
+import binar.finalproject.binair.buyer.data.Constant.initApp
+import binar.finalproject.binair.buyer.databinding.FragmentSplashScreenBinding
 
 class SplashScreenFragment : Fragment() {
+    private lateinit var binding : FragmentSplashScreenBinding
+    private lateinit var sharedPref : SharedPreferences
+    private lateinit var editor : SharedPreferences.Editor
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_splash_screen, container, false)
+        binding = FragmentSplashScreenBinding.inflate(inflater,container,false)
+        sharedPref = requireActivity().getSharedPreferences(initApp, Context.MODE_PRIVATE)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Handler(Looper.myLooper()!!).postDelayed({
+            if(sharedPref.getBoolean("firstRun",true)) {
+                editor.putBoolean("firstRun", false)
+                editor.apply()
+                findNavController().navigate(R.id.action_spalshScreenFragment_to_carouselFragment2)
+            }else{
+                findNavController().navigate(R.id.action_spalshScreenFragment_to_homeFragment)
+            }
+        },2500)
     }
 
 
