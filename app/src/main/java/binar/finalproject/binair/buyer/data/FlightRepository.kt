87dc05e4b5298ec2816b7ce2d8a6379af730edda
Richard Lois <richard.lois@ti.ukdw.ac.id>
2022-12-02 21:15:43 +0700

    Make repo for flight

---
+++ app/src/main/java/binar/finalproject/binair/buyer/data/FlightRepository.kt
@@ -0,0 +1,8 @@
+package binar.finalproject.binair.buyer.data
+
+import binar.finalproject.binair.buyer.data.remote.APIService
+import javax.inject.Inject
+
+class FlightRepository @Inject constructor(var apiService: APIService) {
+
+}
