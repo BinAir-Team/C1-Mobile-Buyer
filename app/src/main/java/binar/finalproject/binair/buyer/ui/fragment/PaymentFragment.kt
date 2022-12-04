96f023d00a0c90833654c8e0d7fac2130b385ada
Richard Lois <richard.lois@ti.ukdw.ac.id>
2022-12-04 17:36:17 +0700

    Add spinner for payment method

--- app/src/main/java/binar/finalproject/binair/buyer/ui/fragment/PaymentFragment.kt
+++ app/src/main/java/binar/finalproject/binair/buyer/ui/fragment/PaymentFragment.kt
@@ -1,21 +1,35 @@
 package binar.finalproject.binair.buyer.ui.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import binar.finalproject.binair.buyer.R
+import binar.finalproject.binair.buyer.databinding.FragmentPaymentBinding
+import binar.finalproject.binair.buyer.ui.adapter.PaymentMethodAdapter
 
 
 class PaymentFragment : Fragment() {
+    private lateinit var binding : FragmentPaymentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_payment, container, false)
+    ): View {
+        binding = FragmentPaymentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initDataSpinner()
+    }
+
+    private fun initDataSpinner() {
+        val arrName = arrayOf("Pilih pembayaran...","Bank BCA", "Bank Mandiri", "Bank BNI", "Bank BRI","Go-Pay","OVO","ShopeePay")
+        val arrPhoto = arrayOf(null,R.drawable.logo_bca, R.drawable.logo_mandiri, R.drawable.logo_bni, R.drawable.logo_bri,R.drawable.logo_gopay,R.drawable.logo_ovo,R.drawable.logo_shopee_pay)
+        binding.spPaymentMethod.adapter = PaymentMethodAdapter(requireContext(), R.layout.item_payment_method, arrName, arrPhoto)
     }
 
 }
