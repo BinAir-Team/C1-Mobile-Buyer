3a415038b128c908d2a18956738f61ab8816f4f3
Richard Lois <richard.lois@ti.ukdw.ac.id>
2022-12-28 20:42:48 +0700

    Formatting date from ISO 8601 format

--- app/src/main/java/binar/finalproject/binair/buyer/data/Utils.kt
+++ app/src/main/java/binar/finalproject/binair/buyer/data/Utils.kt
@@ -11,11 +11,19 @@ import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
 import binar.finalproject.binair.buyer.R
 import java.text.NumberFormat
+import java.text.SimpleDateFormat
 import java.util.*
 
 fun formatRupiah(value: Int): String {
     val formatter = NumberFormat.getCurrencyInstance(Locale("in", "ID"))
     return formatter.format(value)
+}
+
+fun formatDateFromISO(date: String): String {
+    val formatter = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.getDefault())
+    val newDate = formatter.parse(date)
+    val formatter2 = SimpleDateFormat("dd MMMM yyyy", Locale("id", "ID"))
+    return formatter2.format(newDate)
 }
 
 fun makeNotification(title : String, message : String, context: Context) {
