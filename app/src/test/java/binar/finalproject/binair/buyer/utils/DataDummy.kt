14ce7338812636374c415427f487431de57d8fd3
Richard Lois <richard.lois@ti.ukdw.ac.id>
2022-12-31 12:57:06 +0700

    Create unit test for FlightViewModel

--- app/src/test/java/binar/finalproject/binair/buyer/utils/DataDummy.kt
+++ app/src/test/java/binar/finalproject/binair/buyer/utils/DataDummy.kt
@@ -1,12 +1,227 @@
 package binar.finalproject.binair.buyer.utils
 
-import binar.finalproject.binair.buyer.data.response.DataLogin
-import binar.finalproject.binair.buyer.data.response.DataUser
-import binar.finalproject.binair.buyer.data.response.LoginResponse
-import binar.finalproject.binair.buyer.data.response.RegisterUserResponse
+import binar.finalproject.binair.buyer.data.response.*
 
 object DataDummy {
-    fun generateUserLogin() : DataLogin {
+    private val listTicket = mutableListOf(
+        TicketItem(
+            arrivalTime = "13:00",
+            airportTo = "Sultan Hasanuddin International Airport",
+            childPrice = 1200000,
+            adultPrice = 1420000,
+            available = true,
+            currStock = 200,
+            dateEnd = null,
+            type = "oneway",
+            createdAt = "2022-12-23T12: 49: 34.710Z",
+            dateStart = "2023-01-01",
+            airportFrom = "Juanda International Airport",
+            from = "Surabaya",
+            id = "31588def-1557-4a4c-8083-a2a3010790f5",
+            to = "Makassar",
+            initStock = 200,
+            departureTime = "12: 00",
+            updatedAt = "2022-12-23T12: 49: 34.710Z"
+        ),
+        TicketItem(
+            arrivalTime = "13:00",
+            airportTo = "Sultan Hasanuddin International Airport",
+            childPrice = 1200000,
+            adultPrice = 1420000,
+            available = true,
+            currStock = 200,
+            dateEnd = null,
+            type = "oneway",
+            createdAt = "2022-12-23T12: 49: 34.710Z",
+            dateStart = "2023-01-01",
+            airportFrom = "Juanda International Airport",
+            from = "Surabaya",
+            id = "31588def-1557-4a4c-8083-a2a3010790f5",
+            to = "Makassar",
+            initStock = 200,
+            departureTime = "12: 00",
+            updatedAt = "2022-12-23T12: 49: 34.710Z"
+        ),
+        TicketItem(
+            arrivalTime = "13:00",
+            airportTo = "Sultan Hasanuddin International Airport",
+            childPrice = 1200000,
+            adultPrice = 1420000,
+            available = true,
+            currStock = 200,
+            dateEnd = null,
+            type = "oneway",
+            createdAt = "2022-12-23T12: 49: 34.710Z",
+            dateStart = "2023-01-01",
+            airportFrom = "Juanda International Airport",
+            from = "Surabaya",
+            id = "31588def-1557-4a4c-8083-a2a3010790f5",
+            to = "Makassar",
+            initStock = 200,
+            departureTime = "12: 00",
+            updatedAt = "2022-12-23T12: 49: 34.710Z"
+        ),
+        TicketItem(
+            arrivalTime = "13:00",
+            airportTo = "Sultan Hasanuddin International Airport",
+            childPrice = 1200000,
+            adultPrice = 1420000,
+            available = true,
+            currStock = 200,
+            dateEnd = null,
+            type = "oneway",
+            createdAt = "2022-12-23T12: 49: 34.710Z",
+            dateStart = "2023-01-01",
+            airportFrom = "Juanda International Airport",
+            from = "Surabaya",
+            id = "31588def-1557-4a4c-8083-a2a3010790f5",
+            to = "Makassar",
+            initStock = 200,
+            departureTime = "12: 00",
+            updatedAt = "2022-12-23T12: 49: 34.710Z"
+        ),
+        TicketItem(
+            arrivalTime = "13:00",
+            airportTo = "Sultan Hasanuddin International Airport",
+            childPrice = 1200000,
+            adultPrice = 1420000,
+            available = true,
+            currStock = 200,
+            dateEnd = null,
+            type = "oneway",
+            createdAt = "2022-12-23T12: 49: 34.710Z",
+            dateStart = "2023-01-01",
+            airportFrom = "Juanda International Airport",
+            from = "Surabaya",
+            id = "31588def-1557-4a4c-8083-a2a3010790f5",
+            to = "Makassar",
+            initStock = 200,
+            departureTime = "12: 00",
+            updatedAt = "2022-12-23T12: 49: 34.710Z"
+        ),
+    )
+
+    private val listTicketRoundtrip = mutableListOf(
+        TicketItem(
+            arrivalTime = "13:00",
+            airportTo = "Sultan Hasanuddin International Airport",
+            childPrice = 1200000,
+            adultPrice = 1420000,
+            available = true,
+            currStock = 200,
+            dateEnd = null,
+            type = "roundtrip",
+            createdAt = "2022-12-23T12: 49: 34.710Z",
+            dateStart = "2023-01-12",
+            airportFrom = "Juanda International Airport",
+            from = "Surabaya",
+            id = "31588def-1557-4a4c-8083-a2a3010790f5",
+            to = "Makassar",
+            initStock = 200,
+            departureTime = "12: 00",
+            updatedAt = "2022-12-23T12: 49: 34.710Z"
+        ),
+        TicketItem(
+            arrivalTime = "13:00",
+            airportTo = "Sultan Hasanuddin International Airport",
+            childPrice = 1200000,
+            adultPrice = 1420000,
+            available = true,
+            currStock = 200,
+            dateEnd = null,
+            type = "roundtrip",
+            createdAt = "2022-12-23T12: 49: 34.710Z",
+            dateStart = "2023-01-12",
+            airportFrom = "Juanda International Airport",
+            from = "Surabaya",
+            id = "31588def-1557-4a4c-8083-a2a3010790f5",
+            to = "Makassar",
+            initStock = 200,
+            departureTime = "12: 00",
+            updatedAt = "2022-12-23T12: 49: 34.710Z"
+        ),
+        TicketItem(
+            arrivalTime = "13:00",
+            airportTo = "Sultan Hasanuddin International Airport",
+            childPrice = 1200000,
+            adultPrice = 1420000,
+            available = true,
+            currStock = 200,
+            dateEnd = null,
+            type = "roundtrip",
+            createdAt = "2022-12-23T12: 49: 34.710Z",
+            dateStart = "2023-01-12",
+            airportFrom = "Juanda International Airport",
+            from = "Surabaya",
+            id = "31588def-1557-4a4c-8083-a2a3010790f5",
+            to = "Makassar",
+            initStock = 200,
+            departureTime = "12: 00",
+            updatedAt = "2022-12-23T12: 49: 34.710Z"
+        ),
+        TicketItem(
+            arrivalTime = "13:00",
+            airportTo = "Sultan Hasanuddin International Airport",
+            childPrice = 1200000,
+            adultPrice = 1420000,
+            available = true,
+            currStock = 200,
+            dateEnd = null,
+            type = "roundtrip",
+            createdAt = "2022-12-23T12: 49: 34.710Z",
+            dateStart = "2023-01-12",
+            airportFrom = "Juanda International Airport",
+            from = "Surabaya",
+            id = "31588def-1557-4a4c-8083-a2a3010790f5",
+            to = "Makassar",
+            initStock = 200,
+            departureTime = "12: 00",
+            updatedAt = "2022-12-23T12: 49: 34.710Z"
+        ),
+        TicketItem(
+            arrivalTime = "13:00",
+            airportTo = "Sultan Hasanuddin International Airport",
+            childPrice = 1200000,
+            adultPrice = 1420000,
+            available = true,
+            currStock = 200,
+            dateEnd = null,
+            type = "roundtrip",
+            createdAt = "2022-12-23T12: 49: 34.710Z",
+            dateStart = "2023-01-12",
+            airportFrom = "Juanda International Airport",
+            from = "Surabaya",
+            id = "31588def-1557-4a4c-8083-a2a3010790f5",
+            to = "Makassar",
+            initStock = 200,
+            departureTime = "12: 00",
+            updatedAt = "2022-12-23T12: 49: 34.710Z"
+        ),
+    )
+
+    private val listTrav = listOf(
+        TravelerItem(
+            datebirth = "19 Aug 94",
+            idCard = "3692581472589",
+            name = "Tulus",
+            nationality = "Indonesia ",
+            noKtp = "3692581472589",
+            surname = "Tulus",
+            tittle = "Tuan",
+            type = "adult"
+        ),TravelerItem(
+            datebirth = "19 Aug 02",
+            idCard = null,
+            name = "Legowo",
+            nationality = "Indonesia ",
+            noKtp = null,
+            surname = "Legowo",
+            tittle = "",
+            type = "child"
+        )
+    )
+
+    fun generateUserLogin(): DataLogin {
         return DataLogin(
             id = "1",
             firstname = "John",
@@ -20,8 +235,17 @@ object DataDummy {
         )
     }
 
-    fun generateUserRegister() : RegisterUserResponse {
-        val dtUser =  DataUser(id = "1", firstname = "John", lastname = "Doe", email = "johndoe@gmail", gender = "Laki-laki", phone = "08123456789", role = "member", password = "123456")
+    fun generateUserRegister(): RegisterUserResponse {
+        val dtUser = DataUser(
+            id = "1",
+            firstname = "John",
+            lastname = "Doe",
+            email = "johndoe@gmail",
+            gender = "Laki-laki",
+            phone = "08123456789",
+            role = "member",
+            password = "123456"
+        )
         return RegisterUserResponse(
             status = "success",
             message = "Register success",
@@ -29,7 +253,7 @@ object DataDummy {
         )
     }
 
-    fun registerFailedEmailPassEmpty() : RegisterUserResponse {
+    fun registerFailedEmailPassEmpty(): RegisterUserResponse {
         return RegisterUserResponse(
             status = "error",
             message = "Email and password is required",
@@ -37,7 +261,7 @@ object DataDummy {
         )
     }
 
-    fun registerFailedEmailInvalid() : RegisterUserResponse {
+    fun registerFailedEmailInvalid(): RegisterUserResponse {
         return RegisterUserResponse(
             status = "error",
             message = "Email format is invalid",
@@ -45,7 +269,7 @@ object DataDummy {
         )
     }
 
-    fun registerEmailAlreadyExist() : RegisterUserResponse {
+    fun registerEmailAlreadyExist(): RegisterUserResponse {
         return RegisterUserResponse(
             status = "error",
             message = "Email already exist",
@@ -53,7 +277,7 @@ object DataDummy {
         )
     }
 
-    fun registerFailedPassNotEqual() : RegisterUserResponse{
+    fun registerFailedPassNotEqual(): RegisterUserResponse {
         return RegisterUserResponse(
             status = "error",
             message = "Password and confirm password does not match",
@@ -61,7 +285,7 @@ object DataDummy {
         )
     }
 
-    fun loginSuccess() : LoginResponse {
+    fun loginSuccess(): LoginResponse {
         return LoginResponse(
             status = "success",
             message = "Login success",
@@ -69,7 +293,7 @@ object DataDummy {
         )
     }
 
-    fun loginFailedEmailNotFound() : LoginResponse {
+    fun loginFailedEmailNotFound(): LoginResponse {
         return LoginResponse(
             status = "error",
             message = "Email does not exist",
@@ -77,7 +301,7 @@ object DataDummy {
         )
     }
 
-    fun loginFailedPassWrong() : LoginResponse {
+    fun loginFailedPassWrong(): LoginResponse {
         return LoginResponse(
             status = "error",
             message = "Password is incorrect",
@@ -85,11 +309,44 @@ object DataDummy {
         )
     }
 
-    fun loginFailedEmailNotVerified() : LoginResponse {
+    fun loginFailedEmailNotVerified(): LoginResponse {
         return LoginResponse(
             status = "error",
             message = "Email not verified, check your email!",
             data = null
+        )
+    }
+
+    fun searchTicketFound(): List<TicketItem> {
+        return listTicket
+    }
+
+    fun successSearchTicketRoundTrip(): List<TicketItem> {
+        return listTicketRoundtrip
+    }
+
+    fun bookTicketSuccess(): BookingTicketResponse {
+        return BookingTicketResponse(
+            status = 200,
+            msg = "insert data success",
+            data = listOf(
+                DataItem(
+                    ticketsId = "432762ac-0aa1-44ff-81dc-4d0602eac592",
+                    date = "2022-12-30T13:54:23.032Z",
+                    id = "3f1e3539-5937-4503-9cfd-1ba07bf0a129",
+                    quantity = Quantity(
+                        adult = 1,
+                        child = 1
+                    ),
+                    traveler = listTrav,
+                    createdAt = "2022-12-26T14:04:14.892Z",
+                    updatedAt = "2022-12-26T14:04:14.892Z",
+                    amounts = 5660000,
+                    usersId = "2daab424-c4d5-4a15-9e46-8ec20a928e8a",
+                    status = "PENDING PAYMENT",
+                    paymentMethod = "Bank BCA",
+                )
+            )
         )
     }
 }
--- app/src/test/java/binar/finalproject/binair/buyer/viewmodel/FlightViewModelTest.kt
+++ app/src/test/java/binar/finalproject/binair/buyer/viewmodel/FlightViewModelTest.kt
@@ -1,24 +1,108 @@
 package binar.finalproject.binair.buyer.viewmodel
 
-import binar.finalproject.binair.buyer.repository.UserRepository
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import binar.finalproject.binair.buyer.data.model.PostBookingBody
+import binar.finalproject.binair.buyer.data.model.Quantity
+import binar.finalproject.binair.buyer.data.model.TravelerItem
+import binar.finalproject.binair.buyer.data.response.BookingTicketResponse
+import binar.finalproject.binair.buyer.data.response.TicketItem
+import binar.finalproject.binair.buyer.repository.FlightRepository
+import binar.finalproject.binair.buyer.utils.DataDummy
+import binar.finalproject.binair.buyer.utils.getOrAwaitValue
+import org.junit.Assert
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
 import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.junit.MockitoJUnitRunner
 
+@RunWith(MockitoJUnitRunner::class)
 class FlightViewModelTest{
+    @get:Rule
+    val instantExecutorRule = InstantTaskExecutorRule()
+
     @Mock
-    private lateinit var userRepo: UserRepository
-    private lateinit var userVM: UserViewModel
+    private lateinit var flightRepo: FlightRepository
+    private lateinit var flightVM : FlightViewModel
 
     @Before
     fun setUP(){
-        userVM = UserViewModel(userRepo)
+        flightVM = FlightViewModel(flightRepo)
     }
 
     @Test
     fun `Success search ticket`(){
+        val dummyMutLD = MutableLiveData<List<TicketItem>?>()
+        dummyMutLD.value = DataDummy.searchTicketFound()
+        val expectedResponse: LiveData<List<TicketItem>?> = dummyMutLD
+        Mockito.`when`(flightVM.callGetTicketBySearch("Surabaya","Juanda","Makasaar","Sultan Hasanuddin","2023-01-01",null,"oneway")).thenReturn(expectedResponse)
 
+        val actualResp = flightVM.callGetTicketBySearch("Surabaya","Juanda","Makasaar","Sultan Hasanuddin","2023-01-01",null,"oneway").getOrAwaitValue()
+        Mockito.verify(flightRepo).callGetTicketBySearch("Surabaya","Juanda","Makasaar","Sultan Hasanuddin","2023-01-01",null,"oneway")
+        Assert.assertNotNull(actualResp)
     }
 
+    @Test
+    fun `Success search ticket oneway`(){
+        val dummyMutLD = MutableLiveData<List<TicketItem>?>()
+        dummyMutLD.value = DataDummy.searchTicketFound()
+        val expectedResponse: LiveData<List<TicketItem>?> = dummyMutLD
+        Mockito.`when`(flightVM.callGetTicketBySearch("Surabaya","Juanda","Makasaar","Sultan Hasanuddin","2023-01-01","2023-01-10","oneway")).thenReturn(expectedResponse)
+
+        val actualResp = flightVM.callGetTicketBySearch("Surabaya","Juanda","Makasaar","Sultan Hasanuddin","2023-01-01","2023-01-10","oneway").getOrAwaitValue()
+        Mockito.verify(flightRepo).callGetTicketBySearch("Surabaya","Juanda","Makasaar","Sultan Hasanuddin","2023-01-01","2023-01-10","oneway")
+        Assert.assertEquals(expectedResponse.value,actualResp)
+    }
+
+    @Test
+    fun `Success search ticket roundtrip`(){
+        val dummyMutLD = MutableLiveData<List<TicketItem>?>()
+        dummyMutLD.value = DataDummy.successSearchTicketRoundTrip()
+        val expectedResponse: LiveData<List<TicketItem>?> = dummyMutLD
+        Mockito.`when`(flightVM.callGetTicketBySearch("Surabaya","Juanda","Makasaar","Sultan Hasanuddin","2023-01-01","2023-01-10","roundtrip")).thenReturn(expectedResponse)
+
+        val actualResp = flightVM.callGetTicketBySearch("Surabaya","Juanda","Makasaar","Sultan Hasanuddin","2023-01-01","2023-01-10","roundtrip").getOrAwaitValue()
+        Mockito.verify(flightRepo).callGetTicketBySearch("Surabaya","Juanda","Makasaar","Sultan Hasanuddin","2023-01-01","2023-01-10","roundtrip")
+        Assert.assertEquals(expectedResponse.value,actualResp)
+    }
+
+    @Test
+    fun `Success book ticket`(){
+        val listTrav = listOf(
+            TravelerItem(
+                datebirth = "19 Aug 94",
+                idCard = "3692581472589",
+                name = "Tulus",
+                nationality = "Indonesia ",
+                noKtp = "3692581472589",
+                surname = "Tulus",
+                tittle = "Tuan",
+                type = "adult"
+            ), TravelerItem(
+                datebirth = "19 Aug 02",
+                idCard = null,
+                name = "Legowo",
+                nationality = "Indonesia ",
+                noKtp = null,
+                surname = "Legowo",
+                tittle = "",
+                type = "child"
+            )
+        )
+        val dataBooking = PostBookingBody("432762ac-0aa1-44ff-81dc-4d0602eac592",
+            Quantity(1, 1),listTrav)
+        val dummyMutLD = MutableLiveData<BookingTicketResponse>()
+        dummyMutLD.value = DataDummy.bookTicketSuccess()
+        val expectedResponse: LiveData<BookingTicketResponse?> = dummyMutLD
+        Mockito.`when`(flightVM.bookTicket("1",dataBooking)).thenReturn(expectedResponse)
+
+        val actualResp = flightVM.bookTicket("1",dataBooking).getOrAwaitValue()
+        Mockito.verify(flightRepo).bookTicket("1",dataBooking)
+        Assert.assertEquals(expectedResponse.value,actualResp)
+    }
 
 }
