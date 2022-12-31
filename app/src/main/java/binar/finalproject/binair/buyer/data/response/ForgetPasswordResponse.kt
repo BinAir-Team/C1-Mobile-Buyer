d11fb2e424b20148fa21d1b504480f09495ec219
Richard Lois <richard.lois@ti.ukdw.ac.id>
2022-12-31 12:56:27 +0700

    Add forget password implementation

---
+++ app/src/main/java/binar/finalproject/binair/buyer/data/response/ForgetPasswordResponse.kt
@@ -0,0 +1,19 @@
+package binar.finalproject.binair.buyer.data.response
+
+import com.google.gson.annotations.SerializedName
+
+data class ForgetPasswordResponse(
+
+	@field:SerializedName("data")
+	val data: DataForgetPass,
+
+	@field:SerializedName("message")
+	val message: String,
+
+	@field:SerializedName("status")
+	val status: String
+)
+
+data class DataForgetPass(
+	val any: Any? = null
+)
